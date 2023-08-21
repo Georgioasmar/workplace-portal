c3d91c12bf19c7e521c7ce4856e77b79b4d874d1
Georgioasmar <asmargeorgio@gmail.com>
2023-08-21 10:00:00 +0300

    Converted to OOP

---
+++ Client/Form8.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form8 : Form
+    {
+        public Form8()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form8_Load(object sender, EventArgs e)
+        {
+            if (Form1.loggedID != null)
+            {
+                if (Form1.loggedAdmin)
+                {
+                    cb_booker.Items.Add(Form3.fullName);
+                    cb_booker.Items.Add("System Admin");
+                    cb_booker.Text = Form3.fullName;
+                }
+                else
+                {
+                    txt_bookerID.Text = Form1.loggedID;
+                    cb_booker.Items.Add(Form3.fullName);
+                    cb_booker.Text = Form3.fullName;
+                }
+            }
+            else if(Form1.loggedID == null)
+            {
+                cb_booker.Items.Add("System Admin");
+                cb_booker.Text = "System Admin";
+            }
+        }
+    }
+}
